f173960d62cf6509ae7e599c26a473fa945e6f1d
M.Huzaifa200 <mk26408527@gmail.com>
2025-11-06 22:39:48 +0500

    Revert to original fonts

--- app.py
+++ app.py
@@ -11,24 +11,18 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Custom font directory
-custom_font_dir = "fonts"
-custom_fonts = ["Big", "Cosmike", "Graffiti", "Isometric1", "Standard"]
+font_list = ["block", "banner", "standard", "doh", "larry3d"]
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request, ascii_art: str = None):
-    context = {"request": request, "message": "Hello from FastAPI!", "ascii_art": ascii_art, "fonts": custom_fonts}
+    context = {"request": request, "message": "Hello from FastAPI!", "ascii_art": ascii_art, "fonts": font_list}
     return templates.TemplateResponse("index.html", context)
 
 @app.post("/submit", response_class=HTMLResponse)
 async def submit(request: Request, user_text: str = Form(...), font_select: str = Form(...)):
-    if font_select not in custom_fonts:
-        return templates.TemplateResponse("index.html", {"request": request, "message": "Invalid font selected.", "ascii_art": None, "fonts": custom_fonts})
-
     try:
-        custom_font = pyfiglet.Figlet(font=f"{custom_font_dir}/{font_select}.flf")
-        art = custom_font.renderText(user_text)
-        context = {"request": request, "message": "Generated ASCII Art:", "ascii_art": art, "fonts": custom_fonts}
+        art = pyfiglet.figlet_format(user_text, font=font_select)
+        context = {"request": request, "message": "Generated ASCII Art:", "ascii_art": art, "fonts": font_list}
         return templates.TemplateResponse("index.html", context)
     except Exception as e:
-        return templates.TemplateResponse("index.html", {"request": request, "message": f"Error: {e}", "ascii_art": None, "fonts": custom_fonts})
+        return templates.TemplateResponse("index.html", {"request": request, "message": f"Error: {e}", "ascii_art": None, "fonts": font_list})
