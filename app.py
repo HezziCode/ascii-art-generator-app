a92c6098de8c0453292bf1af0168abf0800b9583
M.Huzaifa200 <mk26408527@gmail.com>
2025-11-06 22:12:33 +0500

    Keep only 5 classic fonts

--- app.py
+++ app.py
@@ -13,21 +13,16 @@ templates = Jinja2Templates(directory="templates")
 
 # Custom font directory
 custom_font_dir = "fonts"
-custom_fonts = ["ShadowText", "Big", "Cosmike", "Graffiti", "Isometric1", "Standard"]
+custom_fonts = ["Big", "Cosmike", "Graffiti", "Isometric1", "Standard"]
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request, ascii_art: str = None):
-    font_list = pyfiglet.FigletFont.getFonts() + custom_fonts
-    context = {"request": request, "message": "Hello from FastAPI!", "ascii_art": ascii_art, "fonts": font_list}
+    context = {"request": request, "message": "Hello from FastAPI!", "ascii_art": ascii_art, "fonts": custom_fonts}
     return templates.TemplateResponse("index.html", context)
 
 @app.post("/submit", response_class=HTMLResponse)
 async def submit(request: Request, user_text: str = Form(...), font_select: str = Form(...)):
-    if font_select in custom_fonts:
-        custom_font = pyfiglet.Figlet(font=f"{custom_font_dir}/{font_select}.flf")
-        art = custom_font.renderText(user_text)
-    else:
-        art = pyfiglet.figlet_format(user_text, font=font_select)
-    font_list = pyfiglet.FigletFont.getFonts() + custom_fonts
-    context = {"request": request, "message": "Generated ASCII Art:", "ascii_art": art, "fonts": font_list}
+    custom_font = pyfiglet.Figlet(font=f"{custom_font_dir}/{font_select}.flf")
+    art = custom_font.renderText(user_text)
+    context = {"request": request, "message": "Generated ASCII Art:", "ascii_art": art, "fonts": custom_fonts}
     return templates.TemplateResponse("index.html", context)
